492d19373f77e52317526eb5c4cbbda5270557e3
Curtis Bloomfield <curtbloom@gmail.com>
2019-07-30 19:40:59 -0400

    email-questions-added

---
+++ controllers/ContactController.js
@@ -0,0 +1,31 @@
+const inquirer = require("inquirer");
+const Contact = require("../models").Contact;
+
+module.exports = class ContactController {
+
+  constructor(){
+    this.contacts = [];
+    this.addContactQuestions = [
+       {
+         type: "input",
+         name: "name",
+         message: "Contact's name - ",
+         validate(val){
+           return val !== "";
+         }
+       },
+       {
+         type: "input",
+         name: "email",
+         message: "Contact's email - ",
+         validate(val){
+           return val !== "";
+         }
+       }
+     ];
+  }
+
+  addContact(name, phone, email){
+    return Contact.create({name, phone, email})
+  }
+}
