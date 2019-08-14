ca94198b4919375a7e720f19f3163cbb657f0959
Curtis Bloomfield <curtbloom@gmail.com>
2019-08-14 15:12:13 -0400

    temporary-changes

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,53 +1,110 @@
 const Contact = require("../models").Contact;
 
 module.exports = class ContactController {
+    constructor(){
+        this.contacts = [];
+        this.addContactQuestions = [
+            {
+                type: "input",
+                name: "name",
+                message: "Contact's name - ",
+                validate(val){
+                    return val !== ""
+                }
+            },
+            {
+                type: "input",
+                name: "phone",
+                message: "Contact's phone number - ",
+                validate(val){
+                    return val !== "";
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Contact's email - ",
+                validate(val){
+                    return val !== "";
+                }
+            }
+        ];
+        this.searchQuestions = [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Name of contact to search - ',
+                validate(val){
+                    return val !== "";
+                }
+            }
+        ];
+        this.showContactQuestions = [
+            {
+                type: "list",
+                name: "selected",
+                message: "Please choose from an option below: ",
+                choices: [
+                    "Delete contact",
+                    "Main menu"
+                ]
+            }
+        ];
 
-  constructor(){
-    this.contacts = [];
-    this.addContactQuestions = [
-       {
-         type: "input",
-         name: "name",
-         message: "Contact's name - ",
-         validate(val){
-           return val !== "";
-         }
-       },
+        this.deleteConfirmQuestions = [
+            {
+                type: "confirm",
+                name: "confirmation",
+                message: "are you sure you want to delete this contact?"
+            }
+        ];
+    }
 
-       {
-         type: "input",
-        name: "phone",
-        message: "Contact's phone number - ",
-        validate(val){
-          return val !== "";
-          }
-        },
+    addContact(name, phone, email){
+        return Contact.create({name, phone, email})
+    }
 
-        {
-         type: "input",
-         name: "email",
-         message: "Contact's email - ",
-         validate(val){
-           return val !== "";
-          }
+    getContacts(){
+        return Contact.findAll()
+    }
+
+    iterativeSearch(contacts, target){
+        for(let contact of contacts){
+            if(contact.name.toLowerCase() === target.toLowerCase())
+            return contact;
         }
-     ];
-   }
+        return null;
+    }
 
-  addContact(name, phone, email) {
-    return Contact.create({name, phone, email})
-  }
+    binarySearch(contacts, target){
+        let min = 0;
+        let max = contacts.length-1;
+        let mid;
 
-  getContacts(){
-    return Contact.findAll()
-  }
+        while(min <= max) {
+            mid = Math.floor((min + max) / 2);
+            let currentContact = contacts[mid];
 
-  iterativeSearch(contacts, target){
-   for (let contact of contacts){
-     if (contact.name.toLowerCase() === target.name.toLowerCase()){
-       return contact;
-     }
-   }
-     return null;
-  }
+            if(currentContact.name > target){
+                max = mid - 1;
+            } else if (currentContact.name < target){
+                min = mid - 1;
+            } else {
+                return contacts[mid];
+            }
+        }
+        return null;
+    }
+
+    search(name){
+        return Contact.findOne({
+          where: {name}
+        });
+      }
+
+      delete(id){
+          return Contact.destroy({
+              where: {id}
+          })
+      }
 }
